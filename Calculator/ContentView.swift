d5a7e263adfe1c034d8fccbc542813a357265a74
AmjadRaji <amjadraji70@gmail.com>
2023-10-04 00:23:18 -0400

    Added Buttons

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,7 +7,40 @@
 
 import SwiftUI
 
+enum CalcButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case subtract = "-"
+    case multiply = "x"
+    case divide = "/"
+    case equal = "="
+    case clear = "AC"
+    case decimal = "."
+    case percent = "%"
+    case negative = "-/+"
+}
+
 struct ContentView: View {
+    
+    let buttons: [[CalcButton]] = [
+        
+        [.clear, .negative, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .subtract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal],
+        
+    ]
+    
     var body: some View {
         ZStack{
             
@@ -22,14 +55,33 @@ struct ContentView: View {
                         .font(.system(size: 52))
                         .foregroundColor(.white)
                         
-                }
+                }.padding()
                 
                 // Buttons
+                ForEach(buttons, id: \.self) { row in
+                    HStack{
+                        ForEach(row, id: \.self) { item in
+                            Button(action: {
+                                
+                                
+                            }, label: {
+                                Text(item.rawValue)
+                                    .font(.system(size: 32))
+                                    .frame(width: 70, height: 70)
+                                    .background(Color.orange)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(35)
+                            })}
+                    }
+                }
                 
                 
             }
         }
     }
+    
+    
+    
 }
 
 #Preview {
